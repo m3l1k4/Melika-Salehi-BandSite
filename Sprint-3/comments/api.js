6737b2ce39834318c2ae95a93f21ef1464f112be
m3l1k4 <melikaihelas@gmail.com>
2020-06-11 22:50:44 -0700

    gets default comments API

--- Sprint-3/comments/api.js
+++ Sprint-3/comments/api.js
@@ -2,23 +2,55 @@ let api_key=  "5c4c8003-3483-45f0-bb8a-89ee259fc2ed";
 
 
 getInfo = () => {
-    axios.get("https://project-1-api.herokuapp.com/comments?api_key=5c4c8003-3483-45f0-bb8a-89ee259fc2ed")
+    axios.get("https://project-1-api.herokuapp.com/comments?api_key="+api_key)
         .then(response => {
             //  console.log(response);
-            console.log(response.data[0].name);
+            console.log(response.data[2].name);
             console.log("this is from axios");
+            insertComments(response);
     
         })
-
 
 };
 
 getInfo();
 
 
+function insertComments(commentsResult){
+
+    let nameTag = document.querySelectorAll('.previous-comments__heading--name ');
+    let timeTag = document.querySelectorAll('.previous-comments__heading--time');
+    let commentTag = document.querySelectorAll('.previous-comments__comment');
+
+    for (let i = 0; i < nameTag.length; i++) {
+ 
+        nameTag[i].innerHTML = commentsResult.data[i].name;
+        commentTag[i].innerHTML = commentsResult.data[i].comment;
+        timeTag[i].innerHTML = epoch2Human(commentsResult.data[i].timestamp);
+     }
+
+
+    console.log("this is response");
+
+}
+
+
+function epoch2Human(timeStampString){
+    currentTime= new Date(timeStampString);
+    let timeString = '';
+    timeDay = currentTime.getDate();
+    timeYear = currentTime.getFullYear();
+    timeMonth = currentTime.getMonth();
+    let timeValue = timeString.concat(timeMonth, '/', timeDay, '/', timeYear);
+
+    return timeValue;
+}
+
+
+
 
 postInfo = () => {
-    axios.post("https://project-1-api.herokuapp.com/comments?api_key=5c4c8003-3483-45f0-bb8a-89ee259fc2ed",
+    axios.post("https://project-1-api.herokuapp.com/comments?api_key="+api_key,
     {name: 'potato',
     comment: 'hahaha'
     })
